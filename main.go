3d22afb5e68862b7325a2e1227c76483fef5453c
sky <sky@skydeMacBook-Pro.local>
2024-09-02 17:00:24 +0800

    add 1559 tx signer

--- main.go
+++ main.go
@@ -2,60 +2,10 @@ package main
 
 import (
 	"log"
-	"math/big"
 	"sync"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 )
-
-const (
-	QueueSize = 1000000
-
-	VALUE    = 1
-	GasLimit = 21000
-	GasPrice = 6_000_000_000 // 6gwei
-	ChainId  = 666666
-	ToAddr   = "0xAe95d8DA9244C37CaC0a3e16BA966a8e852Bb6D6"
-
-	TxNumberPerAccount = 1
-)
-
-var (
-	value     = big.NewInt(VALUE)
-	gasLimit  = uint64(GasLimit)
-	gasPrice  = big.NewInt(GasPrice)
-	chainId   = big.NewInt(ChainId)
-	toAddress = common.HexToAddress(ToAddr)
-)
-
-func startTxMaker(accounts []*Account, txQueue chan *types.Transaction) {
-	var data []byte
-	legacyTx := &types.LegacyTx{
-		GasPrice: gasPrice,
-		Gas:      gasLimit,
-		To:       &toAddress,
-		Value:    value,
-		Data:     data,
-	}
-
-	signer := types.NewEIP155Signer(chainId)
-
-	for _, account := range accounts {
-		for i := 0; i < TxNumberPerAccount; i++ {
-			legacyTx.Nonce = account.nonce
-			tx := types.NewTx(legacyTx)
-			signedTx, err := types.SignTx(tx, signer, account.pk)
-			if err != nil {
-				log.Fatal(err)
-			}
-			txQueue <- signedTx
-			account.nonce += 1
-		}
-	}
-
-	close(txQueue)
-}
 
 func main() {
 	accounts := Load()
@@ -68,7 +18,7 @@ func main() {
 	txQueue := make(chan *types.Transaction, QueueSize)
 
 	StartTxSenderPool(wg, txQueue)
-	startTxMaker(accounts, txQueue)
+	start1559TxMaker(accounts, txQueue)
 
 	log.Println("wait finish...")
 	wg.Wait()
---
+++ tx_maker.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+const (
+	QueueSize = 200000
+)
+
+const (
+	VALUE    = 1
+	GasLimit = 21000
+	ChainId  = 666666
+	ToAddr   = "0xAe95d8DA9244C37CaC0a3e16BA966a8e852Bb6D6"
+)
+
+// GasPrice for Legacy tx
+const (
+	GasPrice = 6_000_000_000 // 6gwei
+)
+
+// MaxPriorityFeePerGas for 1559 tx
+const (
+	MaxPriorityFeePerGas = 6_000_000_000
+	BaseFeePerGas        = 1 // should query from the network
+)
+
+const (
+	TxNumberPerAccount = 20
+)
+
+var (
+	chainId   = big.NewInt(ChainId)
+	toAddress = common.HexToAddress(ToAddr)
+	value     = big.NewInt(VALUE)
+	gasLimit  = uint64(GasLimit)
+)
+
+var (
+	gasPrice = big.NewInt(GasPrice)
+)
+
+var (
+	maxPriorityFeePerGas = big.NewInt(MaxPriorityFeePerGas)
+	maxFeePerGas         = big.NewInt(BaseFeePerGas + MaxPriorityFeePerGas)
+)
+
+func startLegacyTxMaker(accounts []*Account, txQueue chan *types.Transaction) {
+	var data []byte
+	legacyTx := &types.LegacyTx{
+		GasPrice: gasPrice,
+		Gas:      gasLimit,
+		To:       &toAddress,
+		Value:    value,
+		Data:     data,
+	}
+
+	signer := types.NewEIP155Signer(chainId)
+
+	for _, account := range accounts {
+		for i := 0; i < TxNumberPerAccount; i++ {
+			legacyTx.Nonce = account.nonce
+			tx := types.NewTx(legacyTx)
+			signedTx, err := types.SignTx(tx, signer, account.pk)
+			if err != nil {
+				log.Fatal(err)
+			}
+			txQueue <- signedTx
+			account.nonce += 1
+		}
+	}
+
+	close(txQueue)
+}
+
+func start1559TxMaker(accounts []*Account, txQueue chan *types.Transaction) {
+	var data []byte
+
+	dynamicFeeTx := &types.DynamicFeeTx{
+		Gas:       gasLimit,
+		GasTipCap: maxPriorityFeePerGas,
+		GasFeeCap: maxFeePerGas,
+		To:        &toAddress,
+		Value:     value,
+		Data:      data,
+	}
+
+	signer := types.NewLondonSigner(chainId)
+
+	for _, account := range accounts {
+		for i := 0; i < TxNumberPerAccount; i++ {
+			dynamicFeeTx.Nonce = account.nonce
+			tx := types.NewTx(dynamicFeeTx)
+			signedTx, err := types.SignTx(tx, signer, account.pk)
+			if err != nil {
+				log.Fatal(err)
+			}
+			txQueue <- signedTx
+			account.nonce += 1
+		}
+	}
+
+	close(txQueue)
+}
